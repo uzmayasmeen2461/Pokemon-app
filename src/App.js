8a9033dad8f6be1b3873b7179a8f0be33633bf1c
uzmayasmeen <uzma2461@gmail.com>
2021-10-30 19:17:54 +0300

    pokemon-app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,124 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { ChakraProvider, Box, theme, Spinner } from '@chakra-ui/react';
+import Search from './components/Search';
+import { fetchPokemon } from './services/Api';
+import PokemonList from './components/PokemonList';
+import PokemonData from './components/PokemonData';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { uuid } from 'uuidv4';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PokemonDetail from './components/PokemonDetail';
 
 function App() {
+  const [pokemon, setPokemon] = React.useState();
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [image, setImage] = useState([]);
+  const [listItems, setListItems] = useState([]);
+  const LOCAL_STORAGE_KEY = 'listItems';
+  const getPokemon = async query => {
+    if (!query) {
+      setError(true);
+      alert('invalid entry');
+      return;
+    }
+    setLoading(true);
+    setTimeout(async () => {
+      const response = await fetchPokemon(query);
+      const results = await response.json();
+      // console.log(results);
+      setPokemon(results);
+      setImage([...image, results.sprites.front_default]);
+
+      setLoading(false);
+    }, 1500);
+  };
+
+  const addPokemonHandler = listItem => {
+    if (listItems.length < 6 && listItem !== null) {
+      setListItems([...listItems, { id: uuid(), ...listItem }]);
+    } else {
+      alert('only 6 pokemons allowed per person');
+    }
+  };
+
+  const removeHandler = id => {
+    const newList = listItems.filter(item => {
+      return item.id !== id;
+    });
+    setListItems(newList);
+  };
+
+  useEffect(() => {
+    const retrivedValues = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(listItems))
+    );
+    if (retrivedValues) setListItems(retrivedValues);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(listItems));
+  }, [listItems]);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+        {/* <Grid minH="100vh"> */}
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Router>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={props => (
+                <Search
+                  {...props}
+                  getPokemon={getPokemon}
+                  addPokemonHandler={addPokemonHandler}
+                />
+              )}
+            />
+
+            <Route
+              path="/list"
+              exact
+              render={props => (
+                <PokemonList
+                  {...props}
+                  listItems={listItems}
+                  image={image}
+                  getPokemon={getPokemon}
+                  removeHandler={removeHandler}
+                />
+              )}
+            />
+            <Route
+              path="/listItem/:id"
+              render={props => <PokemonDetail {...props} image={image} />}
+            />
+          </Switch>
+        </Router>
+
+        {loading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : null}
+        {!loading && pokemon ? (
+          <PokemonData
+            name={pokemon.name}
+            sprite={pokemon.sprites.front_default}
+            abilities={pokemon.abilities}
+            stats={pokemon.stats}
+            types={pokemon.types}
+          />
+        ) : null}
+
+        {/* </Grid> */}
       </Box>
     </ChakraProvider>
   );
---
+++ src/services/Api.js
@@ -0,0 +1,8 @@
+const baseUrl = 'http://pokeapi.co/api/v2';
+const query = {
+  pokemon: 'pokemon',
+};
+
+export async function fetchPokemon(pokemon) {
+  return fetch(`${baseUrl}/${query.pokemon}/${pokemon}`);
+}
